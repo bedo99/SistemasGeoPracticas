65ca7124d98daba91b2a93f22955585b35f742f2
Eduardo Bedolla <bedo99.cf@gmail.com>
2020-05-12 19:16:41 -0500

    Incersion de practica 10 Amigos

--- Practica10/index.js
+++ Practica10/index.js
@@ -46,7 +46,7 @@ const listaloggedout = document.querySelectorAll('.logged-out');
         informacion = new google.maps.InfoWindow;
 
         var pos = { 
-            lat: doc.data().coordenadas.latitude,
+            lat: doc.data().coordenadas.latitud,
             lng: doc.data().coordenadas.longitude
         };
 
